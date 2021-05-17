c8f46e21811b90887e50c59988cff6e23060bfcd
mueller-ma <mueller-ma@users.noreply.github.com>
2021-05-17 15:02:27 +0200

    Add Context.prefs()

--- app/src/main/java/com/github/muellerma/prepaidbalance/utils/ExtensionFunctions.kt
+++ app/src/main/java/com/github/muellerma/prepaidbalance/utils/ExtensionFunctions.kt
@@ -1,19 +1,19 @@
 package com.github.muellerma.prepaidbalance.utils
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.text.format.DateFormat
+import androidx.preference.PreferenceManager
 
 private const val TAG = "ExtensionFunctions"
 
-fun Double.formatAsCurrency(): String {
-    return "%1\$,.2f".format(this)
-}
+fun Double.formatAsCurrency() = "%1\$,.2f".format(this)
 
 fun Long.timestampForUi(context: Context): String {
     return DateFormat.getTimeFormat(context).format(this) + " " +
             DateFormat.getDateFormat(context).format(this)
 }
 
-fun String.isValidUssdCode(): Boolean {
-    return matches("^\\*(\\d)+#$".toRegex())
-}
+fun String.isValidUssdCode() = matches("^\\*(\\d)+#$".toRegex())
+
+fun Context.prefs() = PreferenceManager.getDefaultSharedPreferences(this)
