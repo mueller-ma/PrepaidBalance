36bd8f9ff18a88337ba5b7ab586d32992c8c5a3d
mueller-ma <mueller-ma@users.noreply.github.com>
2022-07-21 16:56:42 +0200

    Add about page
    Fixes #99

---
+++ app/src/main/java/com/github/muellerma/prepaidbalance/ui/CopyToClipboardClickHandler.kt
@@ -0,0 +1,31 @@
+package com.github.muellerma.prepaidbalance.ui
+
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.util.Log
+import android.widget.Toast
+import androidx.preference.Preference
+import com.github.muellerma.prepaidbalance.R
+import com.github.muellerma.prepaidbalance.utils.prefs
+import com.github.muellerma.prepaidbalance.utils.showToast
+
+class CopyToClipboardClickHandler : Preference.OnPreferenceClickListener {
+    override fun onPreferenceClick(preference: Preference): Boolean {
+        val context = preference.context
+        val value = context.prefs().getString(preference.key, "")
+
+        Log.d(TAG, "Copy $value to clipboard")
+
+        val clipboardManager = context.getSystemService(ClipboardManager::class.java)
+        val clip = ClipData.newPlainText(context.getString(R.string.app_name), value)
+        clipboardManager.setPrimaryClip(clip)
+
+        context.showToast(R.string.copied_to_clipboard)
+
+        return true
+    }
+
+    companion object {
+        private val TAG = CopyToClipboardClickHandler::class.java.simpleName
+    }
+}
