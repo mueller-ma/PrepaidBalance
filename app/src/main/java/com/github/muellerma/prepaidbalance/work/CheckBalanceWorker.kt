18ff5bb7a90b086e36d8ffc8b69337ccf5a69040
mueller-ma <mueller-ma@users.noreply.github.com>
2023-02-27 09:33:36 +0100

    Update deprecated code

--- app/src/main/java/com/github/muellerma/prepaidbalance/work/CheckBalanceWorker.kt
+++ app/src/main/java/com/github/muellerma/prepaidbalance/work/CheckBalanceWorker.kt
@@ -97,7 +97,7 @@ class CheckBalanceWorker(
 
                 enqueueUniquePeriodicWork(
                     "work",
-                    ExistingPeriodicWorkPolicy.REPLACE,
+                    ExistingPeriodicWorkPolicy.UPDATE,
                     request
                 )
             }
